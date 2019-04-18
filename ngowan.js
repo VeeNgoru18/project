24264012d7fbc7025027c75dde1aa0881ebbd5fb
Rapando <samson.rapando@betika.com>
2019-04-18 08:32:10 +0300

    added changes to sales

--- ngowan.js
+++ ngowan.js
@@ -1,21 +1,22 @@
 // todo - escape quotes
 const Swal = require('sweetalert2');
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var md5 = require('md5');
 var jspdf = require('jspdf')
 var jspdfAutotable = require('jspdf-autotable')
 
 var categories = [];
 var books = [];
+var booksOnSale = [];
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'username',
-  password : 'password',
-  database : 'project'
+  host: 'localhost',
+  user: 'username',
+  password: 'password',
+  database: 'project'
 });
 
 // functions that run at startup
-$(function() {
+$(function () {
   // $("#bookCategory").select2()
   fetchCategories();
   fetchBooks();
@@ -33,7 +34,7 @@ function fetchCategories() {
     $("#categoryParent").html('<option value="0" selected>-no parent-</option>')
     $("#bookCategory").html('')
     $("#EditBookCategory").html('')
-    for (var result of results){
+    for (var result of results) {
       $("#categoryParent, #bookCategory, #EditBookCategory").append($('<option>', { //used to insert specified content
         value: result.categoryID,
         text: result.categoryName
@@ -47,12 +48,12 @@ function fetchCategories() {
 
 function downloadBooks() {
   var doc = new jspdf();
-    // You can use html:
-    doc.autoTable({html: '#booksTable'});
-    doc.save('table.pdf');
+  // You can use html:
+  doc.autoTable({ html: '#booksTable' });
+  doc.save('table.pdf');
 }
 
-function AddCategory(){
+function AddCategory() {
   var categoryLevel = 1;
   var categoryName = $('#categoryName').val();
   var categoryParent = $('#categoryParent').val();
@@ -85,26 +86,27 @@ function AddCategory(){
 
 // -- Authentication ---
 function login() {
-  var idNO = $('#idNO').val();
-  var password = $('#password').val();
+  var idNO = $('#loginmodal #idNO').val();
+  var password = $('#loginmodal #password').val();
 
-var query = `select * from users where idNO = '${idNO}' and name = '${name}' and password = '${md5(password)}' and active= '1'`
-connection.query(query, function (error, results, fields) {
-  if (error) throw error;
-  if (results.length > 0){
+  var query = `select * from users where idNO = '${idNO}' and password = '${md5(password)}' and active= '1'`
+  console.log(query)
+  connection.query(query, function (error, results, fields) {
+    if (error) throw error;
+    if (results.length > 0) {
 
-    sessionStorage.setItem('user', JSON.stringify(results[0]))
-    $('#loginmodal').modal('hide');
-  }
-  else {
-    Swal.fire({
-      title: 'Error!',
-      text: 'Wrong Credentials',
-      type: 'error',
-      confirmButtonText: 'OK'
-    })
-  }
-})
+      sessionStorage.setItem('user', JSON.stringify(results[0]))
+      $('#loginmodal').modal('hide');
+    }
+    else {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Wrong Credentials',
+        type: 'error',
+        confirmButtonText: 'OK'
+      })
+    }
+  })
 
 }
 // add new user
@@ -115,24 +117,26 @@ function registerUser() {
   var insertRegisterQuery = `insert into users values (NULL, '${userType}', '${md5(password)}', ${active}) `;
 }
 //sales screen
- //function Addsales(){
-  //var sales;
-  //var customerName = $('#customerName').val();
-  //var salesDate = $('#salesDate').val();
-  //var customerContact = $('#customerContact').val();
+//function Addsales(){
+//var sales;
+//var customerName = $('#customerName').val();
+//var salesDate = $('#salesDate').val();
+//var customerContact = $('#customerContact').val();
 
-  //var query = `insert into sales values (NULL, '${sales}'`
+//var query = `insert into sales values (NULL, '${sales}'`
 
 //}
 
-function fetchBooks(){
+function fetchBooks() {
   var fetchBooksQuery = "select books.*, category.categoryName from books inner join category on category.categoryID = books.categoryID";
 
-  connection.query(fetchBooksQuery, function (error, results, fields){
+  connection.query(fetchBooksQuery, function (error, results, fields) {
     if (error) throw error;
     books = results;
+    sessionStorage.setItem('books', JSON.stringify(books))
     $("#bookList").html('');
-    for (var result of results){
+
+    for (var result of results) {
       var tr = $('<tr>');
 
       tr.append($('<td>', {
@@ -171,7 +175,7 @@ function fetchBooks(){
 }
 
 
-function AddBook(){
+function AddBook() {
   var bookName = $('#bookName').val();
   var category = $('#bookCategory').val();
   var quantity = $('#quantity').val();
@@ -180,29 +184,29 @@ function AddBook(){
 
 
 
-if (!bookName.length  || !category.length  || !quantity || !price || !publisher.length){
-  Swal.fire({
-    title: 'Error!',
-    text: 'You must fill all the book details',
-    type: 'error',
-    confirmButtonText: 'Cool'
-  })
-}
-else {
-  var insertBookQuery = `insert into books values (NULL, '${bookName}', '${category}', '${quantity}', '${price}', '${publisher}')`;
-  console.log(insertBookQuery);
-  connection.query(insertBookQuery, function(error, results, fields){
-    if (error) throw error;
+  if (!bookName.length || !category.length || !quantity || !price || !publisher.length) {
     Swal.fire({
-      title: 'success',
-      text: 'Book added successfully!',
-      type: 'success',
-      confirmButtonText: 'OK'
+      title: 'Error!',
+      text: 'You must fill all the book details',
+      type: 'error',
+      confirmButtonText: 'Cool'
     })
-    $("#NewBookModal input").val('')
-    fetchBooks()
-  });
-}
+  }
+  else {
+    var insertBookQuery = `insert into books values (NULL, '${bookName}', '${category}', '${quantity}', '${price}', '${publisher}')`;
+    console.log(insertBookQuery);
+    connection.query(insertBookQuery, function (error, results, fields) {
+      if (error) throw error;
+      Swal.fire({
+        title: 'success',
+        text: 'Book added successfully!',
+        type: 'success',
+        confirmButtonText: 'OK'
+      })
+      $("#NewBookModal input").val('')
+      fetchBooks()
+    });
+  }
 }
 
 function EditBook(bookID) {
@@ -210,7 +214,7 @@ function EditBook(bookID) {
   //2. fill the form with the book details
   // 3. update the book in the db
   var editBookQuery = `select * from books where bookID = '${bookID}' `
-  connection.query(editBookQuery, function(error, results, fields) {
+  connection.query(editBookQuery, function (error, results, fields) {
     if (error) throw error;
     console.log(results);
     book = results[0]
@@ -222,7 +226,7 @@ function EditBook(bookID) {
     $("#EditBookModal").modal('show')
   })
 
-  $("#SaveChangesEditBook").click(function() {
+  $("#SaveChangesEditBook").click(function () {
     var bookName = $('#EditBookName').val();
     var category = $('#EditBookCategory').val();
     var quantity = $('#EditQuantity').val();
@@ -231,29 +235,64 @@ function EditBook(bookID) {
 
 
 
-  if (!bookName.length  || !category.length  || !quantity || !price || !publisher.length){
-    Swal.fire({
-      title: 'Error!',
-      text: 'You must fill all the book details',
-      type: 'error',
-      confirmButtonText: 'Cool'
-    })
-  }
-  else {
-    var updateBookQuery = `update books set bookName='${bookName}', categoryID='${category}', quantity='${quantity}', price='${price}', publisher='${publisher}' where bookID='${bookID}'`
-
-    connection.query(updateBookQuery, function(error, results, fields){
-      if (error) throw error;
+    if (!bookName.length || !category.length || !quantity || !price || !publisher.length) {
       Swal.fire({
-        title: 'success',
-        text: 'Book updated successfully!',
-        type: 'success',
-        confirmButtonText: 'OK'
+        title: 'Error!',
+        text: 'You must fill all the book details',
+        type: 'error',
+        confirmButtonText: 'Cool'
       })
+    }
+    else {
+      var updateBookQuery = `update books set bookName='${bookName}', categoryID='${category}', quantity='${quantity}', price='${price}', publisher='${publisher}' where bookID='${bookID}'`
 
-      fetchBooks()
-    });
-  }
+      connection.query(updateBookQuery, function (error, results, fields) {
+        if (error) throw error;
+        Swal.fire({
+          title: 'success',
+          text: 'Book updated successfully!',
+          type: 'success',
+          confirmButtonText: 'OK'
+        })
+
+        fetchBooks()
+      });
+    }
   })
+
+}
+
+
+// ----------- Sales --------------- //
+function searchBook(searchText) {
+  var books = JSON.parse(sessionStorage.getItem('books'));
+  searchText = searchText.toLowerCase();
+  var results = books.filter(b => b.bookName.toLowerCase().includes(searchText) || b.publisher.toLowerCase().includes(searchText) || b.categoryName.toLowerCase().includes(searchText))
+
+  $("#searchBookSalesResults").html('')
+  for (result of results) {
+    result.qtty = 1;
+    $("#searchBookSalesResults").append(`<li class="list-group-item" ><div class="row"><div class="col-sm-8">${result.bookName}, ${result.publisher} (${result.categoryName}) KES${result.price}</div><div class="col-sm-2"><input placeholder="qtty" id="bookqtty${result.bookID}" type="number" onkeyup="result.qtty = this.value"  value="${result.qtty}" min=1 " class="form-control" /></div><div class="col-sm-2"><input type="button" onclick="addBookToSales(result)" class="btn btn-info btn-sm" value="Add" /></div></div></li>`)
+  }
+
+}
+
+function addBookToSales(result) {
+  // Here, the result includes the information about the book and also qtty which is the number of books someone has chosen to sell
+  // Adding the current book to the list of books on sales
+  booksOnSale.push(result)
+  $("#searchBookSalesInput").val('')
+  $("#searchBookSalesResults").html('')
+  // Here we get the user details from sessionStorage so that we use it in the sales db
+  var userDetails = sessionStorage.getItem('user');
+  var userID = JSON.parse(user).userID;
+  // here, make an insert query to sales table
+
+  for (result of results) {
+    // for each loop, create a query for inserting a sales item
+    // eg `insert into salesitem values (NULL, ${result.bookID}, ${result.qtty})
+  }
+
+  // after you have added, close the modal
 
 }
